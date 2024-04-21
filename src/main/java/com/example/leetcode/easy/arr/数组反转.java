36e29a1169b10411ba61d2b34bbede5f3dabb0c1
程云博 <chengyunbo@gyyx.cn>
2024-04-21 21:02:17 +0800

    feat: 链表

---
+++ src/main/java/com/example/leetcode/easy/arr/数组反转.java
@@ -0,0 +1,34 @@
+package com.example.leetcode.easy.arr;
+
+import java.util.Arrays;
+
+/**
+ * @author chengyunbo@gyyx.cn
+ * @since 2024-04-21
+ */
+
+/*
+* [1, 2, 3, 4, 5, 6]
+* */
+public class 数组反转 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,3,4,5,6};
+        reversalArr(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void reversalArr(int[] arr) {
+        if (arr == null || arr.length == 1) {
+            return;
+        }
+        for (int i = 0, j = arr.length - 1; i < arr.length; i++, j--) {
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/example/leetcode/medium/BM2链表内指定区间反转.java
@@ -0,0 +1,52 @@
+package com.example.leetcode.medium;
+
+import java.util.Objects;
+
+/**
+ * @author chengyunbo@gyyx.cn
+ * @since 2024-04-21
+ */
+public class BM2链表内指定区间反转 {
+
+    public static class ListNode{
+        private int val;
+
+        private ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ListNode listNode = (ListNode) o;
+            return val == listNode.val && Objects.equals(next, listNode.next);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(val, next);
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+    }
+
+    /*
+    * 链表指定区间反转
+    * */
+    public ListNode reverseBetween (ListNode head, int m, int n) {
+        if (head == null || (m <= 0 && n <=0)) {
+            return null;
+        }
+
+    }
+}
